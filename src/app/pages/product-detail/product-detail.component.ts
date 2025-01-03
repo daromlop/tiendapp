6b3e6a7194e9f7179e7ffdbc01fddd2d39844321
unknown <davirck@gmail.com>
2025-01-03 21:41:43 +0100

    Cambio assets

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -5,6 +5,7 @@ import { ApiService } from '../../services/api.service';
 import { IProduct } from '../../models/product.model';
 import { Review } from '../../models/reviews.model';
 import { ReviewService } from '../../services/review.service';
+import jsonData from '../../assets/data/reviews.json';
 
 @Component({
   selector: 'app-product-detail',
@@ -16,11 +17,10 @@ import { ReviewService } from '../../services/review.service';
 export class ProductDetailComponent {
   loading: boolean = true;
   public product?: IProduct;
-  reviews: Review[] = [];
+  reviews: Review[] = jsonData;
 
   private _route = inject(ActivatedRoute);
   private _apiService = inject(ApiService);
-  private _reviewService = inject(ReviewService);
 
   ngOnInit(): void {
     this._route.params.subscribe((params) => {
@@ -29,19 +29,5 @@ export class ProductDetailComponent {
         this.loading = false;
       });
     });
-    this._reviewService.getReviews().subscribe((data) => {
-      this.reviews = data;
-    });
-  }
-
-  // Genera un arreglo con la cantidad de estrellas llenas
-  getStars(rating: number): number[] {
-    return Array(rating).fill(0); // Retorna un arreglo de tamaño `rating`
-  }
-
-  // Genera un arreglo con la cantidad de estrellas vacías
-  getEmptyStars(rating: number): number[] {
-    const maxStars = 5;
-    return Array(maxStars - rating).fill(0); // Retorna el resto de las estrellas
   }
 }
